a8db99c184ba0bb946539a2b6646443deed2544f
ematty246 <ematty2006@gmail.com>
2025-06-02 07:44:53 +0530

    Added Admin entity and repository

---
+++ connect/src/main/java/com/zidio/connect/admin/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.zidio.connect.admin.repository;
+
+import com.zidio.connect.admin.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByEmail(String email);
+}
